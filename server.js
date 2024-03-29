628e9a5899417fb04c164c2010a79cedba21161a
bmage95 <117807726+bmage95@users.noreply.github.com>
2024-03-29 23:24:06 +0530

    random recommendation page done

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
-const dataFilePath = path.join(__dirname, 'data.json');
+const PORT = 3001;
+const recipeFilePath = path.join(__dirname, 'recipe.json');
 
 // Function to read data from JSON file
-function readDataFromFile() {
+function readDataFromFile(filePath) {
   try {
-    const data = fs.readFileSync(dataFilePath, 'utf8');
+    const data = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(data);
   } catch (error) {
     console.error('Error reading data from file:', error);
@@ -16,38 +16,30 @@ function readDataFromFile() {
   }
 }
 
-// Function to write data to JSON file
-function writeDataToFile(data) {
-  try {
-    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
-    console.log('Data written to file successfully');
-  } catch (error) {
-    console.error('Error writing data to file:', error);
-  }
-}
-
 // Create a basic HTTP server
 const server = http.createServer((req, res) => {
-  if (req.url === '/data' && req.method === 'GET') {
-    // Read data from file and send as response
-    const jsonData = readDataFromFile();
+  if (req.url === '/random' && req.method === 'GET') {
+    // Read data from recipe file, select a random recipe, and send as response
+    const recipes = readDataFromFile(recipeFilePath);
+    const randomIndex = Math.floor(Math.random() * recipes.length);
+    const randomRecipe = recipes[randomIndex];
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(jsonData));
-  } else if (req.url === '/data' && req.method === 'POST') {
-    // Receive data from client, append to file, and send back updated data
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString(); // convert Buffer to string
+    res.end(JSON.stringify(randomRecipe));
+  } 
+  else if (req.url === '/today.html' && req.method === 'GET') {
+    // Serve the today.html file
+    const filePath = path.join(__dirname, 'today.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
     });
-    req.on('end', () => {
-      const newData = JSON.parse(body);
-      const currentData = readDataFromFile();
-      currentData.push(newData);
-      writeDataToFile(currentData);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(currentData));
-    });
-  } else {
+  } 
+  else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
